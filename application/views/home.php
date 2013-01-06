154c4d5f6add9dcc29ec151dbb97e9cf99af73b2
Weilong Ding <tonyaziten@gmail.com>
2013-01-06 17:33:05 +1100

    trivial change

--- application/views/home.php
+++ application/views/home.php
@@ -25,11 +25,17 @@
         <div class="navbar-inner">
           <ul class="nav nav-stacked">
             <li><a href="#">订单管理</a></li>
+            <li class="divider"></li>
             <li><a href="#">进货管理</a></li>
+            <li class="divider"></li>
             <li><a href="#">账目管理</a></li>
+            <li class="divider"></li>
             <li><a href="#">客户管理</a></li>
+            <li class="divider"></li>
             <li><a href="#">供应商管理</a></li>
+            <li class="divider"></li>
             <li><a href="#">商品管理</a></li>
+            <li class="divider"></li>
             <li><a href="#">报表查看</a></li>
           </ul>
         </div>
