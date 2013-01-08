dfd88f46fa6cbe9f1f50621a5c09d5a5a34abdb2
Weilong Ding <tonyaziten@gmail.com>
2013-01-08 14:18:41 +1100

    changed template

--- application/views/includes/navigation.php
+++ application/views/includes/navigation.php
@@ -1,23 +1,22 @@
-<div class = "row">
-    <div class = "span2">
-      <div class="navbar">
-        <div class="navbar-inner">
-          <ul class="nav nav-stacked">
-            <li><a href="#">订单管理</a></li>
-            <li class="divider"></li>
-            <li><a href="#">进货管理</a></li>
-            <li class="divider"></li>
-            <li><a href="#">账目管理</a></li>
-            <li class="divider"></li>
-            <li><a href="#">客户管理</a></li>
-            <li class="divider"></li>
-            <!--Call page controller to load supplier management view -->
-            <li><a href=<?php echo base_url().'page?page=test' ?>>供应商管理</a></li>
-            <li class="divider"></li>
-            <li><a href="#">商品管理</a></li>
-            <li class="divider"></li>
-            <li><a href="#">报表查看</a></li>
-          </ul>
-        </div>
-      </div>
+<div class = "span2">
+  <div class="navbar">
+    <div class="navbar-inner">
+      <ul class="nav nav-stacked">
+        <li><a href="#">订单管理</a></li>
+        <li class="divider"></li>
+        <li><a href="#">进货管理</a></li>
+        <li class="divider"></li>
+        <li><a href="#">账目管理</a></li>
+        <li class="divider"></li>
+        <li><a href="#">客户管理</a></li>
+        <li class="divider"></li>
+        <!--Call page controller to load supplier management view -->
+        <li><a href=<?php echo base_url().'page?page=test' ?>>供应商管理</a></li>
+        <li class="divider"></li>
+        <li><a href="#">商品管理</a></li>
+        <li class="divider"></li>
+        <li><a href="#">报表查看</a></li>
+      </ul>
     </div>
+  </div>
+</div>
