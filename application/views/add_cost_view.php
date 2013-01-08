1c8f5db6da16d15e33f44fc825c176e12a9f7d80
Weilong Ding <tonyaziten@gmail.com>
2013-01-08 17:00:13 +1100

    add supplied_goods_view

--- application/views/add_cost_view.php
+++ application/views/add_cost_view.php
@@ -13,7 +13,7 @@
     <label>描述</label>
     <textarea rows="5"></textarea><br />
     <button type="submit" class="btn btn-primary">确认</button>
-    <button type="submit" class="btn btn-warning">清空</button>
+    <button type="submit" class="btn">清空</button>
   </fieldset>
 </form>
 </div>
