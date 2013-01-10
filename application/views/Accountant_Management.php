cba564e288cce3a1821de5fbc31d73434d935fda
azrealzeon <yzhang810@gmail.com>
2013-01-10 12:08:39 +1100

    edit accountant_management file and some small change in another file

--- application/views/Accountant_Management.php
+++ application/views/Accountant_Management.php
@@ -1,15 +1,52 @@
     <style>
-		body{
-			margin:auto 0px;
-		}
-		.content{
+	/* view payment, viewbalance, cost */
+		.viewpayment,.costpayment,.viewbalance
+		{
 			width:100%;
 			height:auto;
+			overflow:hidden;
 		}
-		.span10
+		.costpayment,.viewbalance
 		{
-			width:83%;
+			height:0px;
 		}
+		.titlebar
+		{
+			width:100%;
+			height:40px;
+		}
+		.company,.retail
+		{
+			line-height:20px;
+			width:40px;
+			float:left;
+			margin-left:10px;
+			padding-left:12px;
+			background:#000;
+			color:#FFF;
+			cursor:pointer;
+		}
+		.retail
+		{
+			background:#fff;
+			color:#000;
+		}
+		.paymentcontrol, .viewpaymentlist, .companypayment, .retailpayment
+		{
+			width:100%;
+			height:auto;
+			overflow:hidden;
+			margin-top:5px;
+		}
+		.paymentcontrol
+		{
+			height:226px;
+		}
+		.retailpayment
+		{
+			height:0px;
+		}
+		
 	</style>
      <div class="content">
          <div class = "row" >
@@ -25,9 +62,9 @@
                     <li class="divider"></li>
                     <li><a href="<?php echo base_url().'page?page=accountant_management' ?>">账目管理</a>
                     	<ul>
-                        	<li><a href="#">账目信息查询</a></li>
-                            <li><a href="#">成本添加</a></li>
-                            <li><a href="#">余额查询</a></li>
+                        	<li><a href="#" id='viewpayment'>账目信息查询</a></li>
+                            <li><a href="#" id='costpayment'>成本添加</a></li>
+                            <li><a href="#" id='viewbalance'>余额查询</a></li>
                         </ul>
                     </li>
                     <li class="divider"></li>
@@ -51,9 +88,115 @@
               </div>
             </div>
             <div class = "span10">
-            	<div class="">
+            	<div class="viewpayment">
+                	<p><h3>客户账户信息</h3></p>
+                	<div class="titlebar">
+                    	<div class="company" id="company">
+                        	<h5>公司</h5>
+                        </div>
+                        <div class="retail" id="retail">
+                        	<h5>零售</h5>
+                        </div>
+                    </div>
+                    <div class="paymentcontrol">
+                    	<div class="companypayment">
+                        	<form method="post">
+                            	<table>
+                                	<tr><td>公司： <select name="company">
+                                    </select>
+                                    </td></tr>
+                                    <tr><td>账户余额： <input type='text' readonly="readonly" /></td></tr>
+                                    <tr><td>本次支付金额： <input type='text' /></td></tr>
+                                    <tr><td>支付方式： <select name="paymentoption"></select></td></tr>
+                                    <tr><td><input type="button" id="companysubmit" value="入账" /></td></tr>
+                                </table> 
+                            </form>
+                        </div>
+                        <div class="retailpayment">
+                        	<form method="post">
+                            	<table>
+                                	<tr><td>零售金额： <input type="text" name="retailamount"  /></td></tr>
+                                    <tr><td><input type="button" id="retailsubmit" value="入账" /></td></tr>
+                                </table>
+                            </form>
+                        </div>
+                        <script language="javascript" type="text/javascript">
+							//control the change from companypayment to retailpayment
+							//control the change from retailpayment to companypayment
+							$(document).ready(function(e) {
+                                $('#company').click(function(e) {
+									$('#company').attr('style','background:#000;color:#fff;');
+									$('#retail').attr('style','background:#fff;color:#000;');
+									$('.retailpayment').css('height','0px');
+                                    $('.companypayment').css('height','216px');
+                                });
+								$('#retail').click(function(e) {
+									$('#company').attr('style','background:#fff;color:#000;');
+									$('#retail').attr('style','background:#000;color:#fff;');
+									$('.companypayment').css('height','0px');
+                                    $('.retailpayment').css('height','216px');
+									
+                                });
+                            });
+						</script>
+                    </div>
+                    <div class="viewpaymentlist">
+                    	<p><h5>明细查询</h5></p>
+                        <div class="paymentlist">
+                        	<p><form method="post">
+                            	下单日期： <input type="date" name='starttime' /> 到 <input type="date" name='endtime' /><br />
+                                <input type="button" id="checklist" value="明细查询" /><br />
+                            </form></p>
+                            <div class="listdetail">
+                            	<table>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="costpayment">
+                	<p><h3>成本预算</h3></p>
+                    <form>
+                      <fieldset>
+                        <legend>添加成本</legend>
+                        <label>成本类型</label>
+                        <select>
+                            <option>Salary</option>
+                            <option>Other<option>
+                        </select><br />
+                        <label>金额</label>
+                        <!-- the type of input needs to be validated at back-end or using ajax -->
+                        <input type="text"></input><br />
+                        <label>描述</label>
+                        <textarea rows="5"></textarea><br />
+                        <button type="submit" class="btn btn-primary">确认</button>
+                        <button type="submit" class="btn">清空</button>
+                      </fieldset>
+                    </form>
+                </div>
+                <div class="viewbalance">
+                	<p><h3>客户账户余额查询</h3></p>
                 </div>
             </div>
+             <script language="javascript" type="text/javascript">
+					$(document).ready(function(e) {
+                        $('#viewpayment').click(function(e) {
+							$('.viewbalance').animate({height:'0px'},"fast");
+							$('.costpayment').animate({height:'0px'},"fast");
+                            $('.viewpayment').animate({height:'100%'},"slow");					
+                        });
+						$('#costpayment').click(function(e) {
+                            $('.viewpayment').animate({height:'0px'},"fast");
+							$('.viewbalance').animate({height:'0px'},"fast");
+                            $('.costpayment').animate({height:'100%'},"slow");
+                        });
+						$('#viewbalance').click(function(e) {
+						    $('.viewpayment').animate({height:'0px'},"fast");
+							$('.costpayment').animate({height:'0px'},"fast");
+                            $('.viewbalance').animate({height:'100%'},"slow");
+                        });
+                    });
+				</script>
          </div>
          
      </div>
