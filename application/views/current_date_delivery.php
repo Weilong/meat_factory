6765f0cf715211c819fcdd3e3f9bc9081330b3ce
azrealzeon <yzhang810@gmail.com>
2013-02-08 16:27:20 +1100

    separated delivery print table

--- application/views/current_date_delivery.php
+++ application/views/current_date_delivery.php
@@ -38,7 +38,7 @@ body
 #delivery_detail
 {
 	width:100%;
-	height:auto;
+	height:590px;
 	padding:20px;
 	position:relative;
 	top:0px;
@@ -71,76 +71,112 @@ body
 <div id="delivery_date">
 <h2><?php echo $currentdate ?></h2>
 </div>
+<?php
+    $productrownumber = count($getproductlist);
+    $baselinecontent = 10;
+    $numberoftable = ceil($productrownumber/$baselinecontent);
+    $a=0; /* loop for create new table */
+	$b=0; /* separate product lsit */
+    for($a=0;$a<$numberoftable;$a++)
+    {
+?>
 <div id="delivery_detail">
-<table id="detail" border="1">
-<tr><td><h4>Company&nbsp; / &nbsp; Product</h4></td>
-<?php
-	/*
-		list all deliveried goods 
-	*/
-	$productrownumber = count($getproductlist);
-	$i=0;
-	for($i=0;$i<$productrownumber;$i++)
-	{
-?>
-         	<td><?php echo $getproductlist[$i];?></td>
-<?php
-	}
-?>
-</tr>
-<?php
-	/*
-		list the deliveried company
-	*/
-    $companyrownumber = count($getcompanylist);
-	$j=0;
-	for($j=0;$j<$companyrownumber;$j++)
-	{
-?>
-	<tr><td><?php echo $getcompanylist[$j];?></td>
-    	
-    	<?php
-			/*
-				list all deliveried goods' qty
-			*/
-			foreach($gettoprint as $printrow)
-			{
-				$companyname = $printrow['companyname'];
-				$productarray = $printrow['deliverydetail'];
-				if($getcompanylist[$j]==$companyname)
-				{
-					for($i=0;$i<$productrownumber;$i++)
-					{
-						foreach($productarray as $deliveryrow)
-						{
-							if($getproductlist[$i]==$deliveryrow['productname'])
-							{
-								?>
-                                <td><?php echo $deliveryrow['qty']; ?></td>
-                                <?php
-								break;
-							}
-						}
-						if($getproductlist[$i]!=$deliveryrow['productname'])
-						{
-							?>
-                            <td></td>
-                            <?php
-						}
-					}
-				}
-			}
-		?>
-    
-    
-    
-    </tr>
-<?php
-	}
-?>
+		<p></p>
+        <p></p>
 
-</table>
+        <table id="detail" border="1">
+            <tr><td><h4>Company&nbsp; / &nbsp; Product</h4></td>
+            <?php
+                /*
+                    list all deliveried goods 
+                */
+                $tempproductarray = array();
+               	$i=0; 
+                while($i<10)
+                {
+					if($b<$productrownumber)
+					{
+            ?>
+                        <td><?php echo $getproductlist[$b];?></td>
+                        
+            <?php
+						array_push($tempproductarray,$getproductlist[$b]);
+					}
+					else
+					{
+						break;
+					}
+						$b++;
+            			$i++;
+                }
+				$b++;
+            ?>
+            </tr>
+            <?php
+                /*
+                    list the deliveried company
+                */
+                $companyrownumber = count($getcompanylist);
+                $j=0;
+                for($j=0;$j<$companyrownumber;$j++)
+                {
+            ?>
+                <tr><td><?php echo $getcompanylist[$j];?></td>
+                    
+                    <?php
+                        /*
+                            list all deliveried goods' qty
+                        */
+                        foreach($gettoprint as $printrow)
+                        {
+                            $companyname = $printrow['companyname'];
+                            $productarray = $printrow['deliverydetail'];
+                            if($getcompanylist[$j]==$companyname)
+                            {
+                                /* 循环搜索各个公司是否有符合的商品并且显示数量*/
+								$i=0;
+								$k=count($tempproductarray);
+								while($i<$k)
+								{
+									$tempname = $tempproductarray[$i];
+									$productname="";
+									foreach($productarray as $goodrow)
+									{
+										$productname=$goodrow['productname'];
+										if($tempname==$goodrow['productname'])
+										{
+											?>
+                                            <td><?php echo $goodrow['qty']; ?></td>
+                                            <?
+											break;
+										}
+									}
+									if($tempname!=$productname)
+									{
+										?>
+                                        <td></td>
+                                        <?php
+									}
+										//对于该表格下的的所有产品进行比
+									$i++;
+								}
+								
+                            }
+                        }
+                    ?>
+                </tr>
+            <?php
+					
+                }
+            ?>
+        </table>
+		
 </div>
+		<p></p>
+        <p></p>
+<?php
+    }
+?>
 <div id="copyright_area">
 	<p>&copy; 2013 SH & CJ QUALITY MEATS & POULTRY</p>
     <p>301 Liverpool Road, Ashfield, NSW</p>
